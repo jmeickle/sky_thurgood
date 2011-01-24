d52fe46f17d60e9bec1f2b7a8f2b06d481df832d
Eronarn Palazzo <eronarn@gmail.com>
2011-01-24 03:43:50 -0500

    Changes to templates and CSS.

--- templates/overrides/node.tpl.php
+++ templates/overrides/node.tpl.php
@@ -10,12 +10,23 @@
  */
 ?>
 <div<?php print $node_attributes; ?>>
+<?php if ($teaser): ?>
+    <div class="meta">
+      <?php if (!$page && $title): ?>
+        <h2><a href="<?php print $node_url; ?>" title="<?php print $title; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+<?php endif; ?>
   <div class="content clearfix">
     <?php print $content; ?>
   </div>
+<?php if ($teaser): ?>
+    </div>
+<?php endif; ?>
+<?php if (!$teaser): ?>
   <?php if ($links): ?>
     <div class="links">
       <?php print $links; ?>
     </div>
   <?php endif; ?>
+<?php endif; ?>
 </div>
